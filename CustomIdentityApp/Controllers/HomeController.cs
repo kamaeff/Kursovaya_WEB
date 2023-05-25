72df3d2490fa71159acfd799269e7c5701b70ece
kamaeff <kamaeff2@gmail.com>
2023-05-25 14:38:44 +0300

    first commit

---
+++ CustomIdentityApp/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using CustomIdentityApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CustomIdentityApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private ApplicationContext db;
+
+        public HomeController(ILogger<HomeController> logger, ApplicationContext context)
+        {
+            _logger = logger;
+            db = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cars()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Orders()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Orders(Orders order)
+        {
+            db.Orders.Add(order);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CustomIdentityApp/Models/Orders.cs
@@ -0,0 +1,9 @@
+﻿namespace CustomIdentityApp.Models
+{
+    public class Orders
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Comment { get; set; }
+    }
+}
